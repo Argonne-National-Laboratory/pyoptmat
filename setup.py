32f38df4cf6783762447e635887749ed62ca80aa
Mark C. Messner <messner@anl.gov>
2022-03-01 13:40:24 -0600

    Version 1.1.2 correcting upload script

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup (
     # Name of the project
     name = 'pyoptmat',
     # Version
-    version = '1.1.1',
+    version = '1.1.2',
     # One line-description
     description = "Statistical inference for material models",
     # README
