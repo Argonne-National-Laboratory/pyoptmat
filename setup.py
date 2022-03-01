f53f4dfa3cf278d6d0bb15083df86f70b537b555
Mark C. Messner <messner@anl.gov>
2022-03-01 13:34:11 -0600

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup (
     # Name of the project
     name = 'pyoptmat',
     # Version
-    version = '1.1.0',
+    version = '1.1.1',
     # One line-description
     description = "Statistical inference for material models",
     # README
