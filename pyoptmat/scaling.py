e1ac7c4f6e84af9462befd7bbe6e255f729d8593
Mark C. Messner <messner@anl.gov>
2022-04-21 10:09:29 -0500

    Need to format your code mark...

--- pyoptmat/scaling.py
+++ pyoptmat/scaling.py
@@ -9,78 +9,84 @@
 
 import torch
 
+
 class ScalingFunction:
     """
-        Common superclass for scaling functions, defines required 
-        methods.
+    Common superclass for scaling functions, defines required
+    methods.
     """
+
     def __call__(self, x):
         """
-            Passes through a function to the `scale` method
+        Passes through a function to the `scale` method
 
-            Args:
-                x (torch.tensor):   scaled parameter values
+        Args:
+            x (torch.tensor):   scaled parameter values
         """
         return self.scale(x)
 
     def scale(self, x):
         """
-            Converts the scaled parameter values to the actual
-            parameter scale
+        Converts the scaled parameter values to the actual
+        parameter scale
 
-            Args:
-                x (torch.tensor):   scaled parameter values
+        Args:
+            x (torch.tensor):   scaled parameter values
         """
         raise NotImplementedError("Method pure virtual")
 
     def unscale(self, x):
         """
-            Converts the unscaled parameter values into the
-            scaled values, for initialization
+        Converts the unscaled parameter values into the
+        scaled values, for initialization
 
-            Args:
-                x (torch.tensor):   unscaled parameter values
+        Args:
+            x (torch.tensor):   unscaled parameter values
         """
         raise NotImplementedError("Method pure virtual")
 
-    def scale_scale(self, x):
+    def scale_stat(self, loc, scale):
         """
-            Convert a scaled standard deviation to the unscaled
-            value in the actual units
+        Provide information on the actual parameter distribution,
+        assuming the scaled properties are normally distributed
+        and parameterized by loc and scale.
 
-            Args:
-                x (torch.tensor):   scaled standard deviation
+        Args:
+            loc (torch.tensor):     location of the scaled parameters
+            scale (torch.tensor):   scale of the scaled parameters
         """
         raise NotImplementedError("Method pure virtual")
+
 
 class BoundedScalingFunction(ScalingFunction):
     """
-        Scaling function where the unscaled parameters are
+    Scaling function where the unscaled parameters are
 
-        .. math::
+    .. math::
 
-            y = x \\left(u - l \\right) + l
+        y = x \\left(u - l \\right) + l
 
-        where :math:`l` is the lower bound on the parameters
-        and :math:`u` is the upper bound.
+    where :math:`l` is the lower bound on the parameters
+    and :math:`u` is the upper bound.
 
-        Args:
-            l (torch.tensor):   lower bound on the parameters
-            u (torch.tensor):   upper bound on the parameter
-            clamp (optional):   if True also clip the values to remain in range
+    Args:
+        l (torch.tensor):   lower bound on the parameters
+        u (torch.tensor):   upper bound on the parameter
+        clamp (optional):   if True also clip the values to remain in range
     """
-    def __init__(self, l, u, clamp = True):
+
+    def __init__(self, l, u, clamp=True):
         self.l = l
         self.u = u
         self.clamp = clamp
 
     def scale(self, x):
         """
-            Converts the scaled parameter values to the actual
-            parameter scale
+        Converts the scaled parameter values to the actual
+        parameter scale
 
-            Args:
-                x (torch.tensor):   scaled parameter values
+        Args:
+            x (torch.tensor):   scaled parameter values
         """
         if self.clamp:
             xp = torch.clamp(x, 0.0, 1.0)
@@ -91,94 +97,98 @@ class BoundedScalingFunction(ScalingFunction):
 
     def unscale(self, x):
         """
-            Converts the unscaled parameter values into the
-            scaled values, for initialization
+        Converts the unscaled parameter values into the
+        scaled values, for initialization
 
-            Args:
-                x (torch.tensor):   unscaled parameter values
+        Args:
+            x (torch.tensor):   unscaled parameter values
         """
-        return (x - self.l) / (self.u - self.l) 
+        return (x - self.l) / (self.u - self.l)
 
     def scale_stat(self, loc, scale):
         """
-            Print some information about how the normally-distributed
-            scaled parameters transform into "real" parameter space
+        Print some information about how the normally-distributed
+        scaled parameters transform into "real" parameter space
 
-            Args:
-                loc (torch.tensor):     scaled loc
-                scale (torch.tensor):   scaled scale
+        Args:
+            loc (torch.tensor):     scaled loc
+            scale (torch.tensor):   scaled scale
         """
         mean = self.scale(loc)
-        std = scale*(self.u - self.l)
-        
+        std = scale * (self.u - self.l)
+
         msg = ""
         msg += "Normal distribution with:\n"
         msg += "\tloc:\n"
-        msg += "\t"+str(mean) + "\n"
+        msg += "\t" + str(mean) + "\n"
         msg += "\tscale:\n"
-        msg += "\t"+str(std)
+        msg += "\t" + str(std)
 
         return msg
 
+
 class LogBoundedScalingFunction(ScalingFunction):
     """
-        Scaling function where the unscaled parameters are
+    Scaling function where the unscaled parameters are
 
-        .. math::
+    .. math::
 
-            y = l**(1-x)*u**x
+        y = l**(1-x)*u**x
 
-        where :math:`l` is the lower bound on the parameters
-        and :math:`u` is the upper bound.
+    where :math:`l` is the lower bound on the parameters
+    and :math:`u` is the upper bound.
 
-        That is, the natural parameter is log transformed and then
-        scaled between two (log transformed) bounds
+    That is, the natural parameter is log transformed and then
+    scaled between two (log transformed) bounds
 
-        The resulting distribution is log normal
+    The resulting distribution is log normal
 
-        Args:
-            l (torch.tensor):   lower bound on the parameters
-            u (torch.tensor):   upper bound on the parameter
-            clamp (optional):   if True also clip the values to remain in range
+    Args:
+        l (torch.tensor):   lower bound on the parameters
+        u (torch.tensor):   upper bound on the parameter
+        clamp (optional):   if True also clip the values to remain in range
     """
-    def __init__(self, l, u, clamp = True):
+
+    def __init__(self, l, u, clamp=True):
         self.l = l
         self.u = u
         self.clamp = clamp
 
     def scale(self, x):
         """
-            Converts the scaled parameter values to the actual
-            parameter scale
+        Converts the scaled parameter values to the actual
+        parameter scale
 
-            Args:
-                x (torch.tensor):   scaled parameter values
+        Args:
+            x (torch.tensor):   scaled parameter values
         """
         if self.clamp:
             xp = torch.clamp(x, 0.0, 1.0)
         else:
             xp = x
 
-        return self.l**(1-x)*self.u**x
+        return self.l ** (1 - xp) * self.u**xp
 
     def unscale(self, x):
         """
-            Converts the unscaled parameter values into the
-            scaled values, for initialization
+        Converts the unscaled parameter values into the
+        scaled values, for initialization
 
-            Args:
-                x (torch.tensor):   unscaled parameter values
+        Args:
+            x (torch.tensor):   unscaled parameter values
         """
-        return (torch.log(x) - torch.log(self.l)) / (torch.log(self.u) - torch.log(self.l)) 
+        return (torch.log(x) - torch.log(self.l)) / (
+            torch.log(self.u) - torch.log(self.l)
+        )
 
     def scale_stat(self, loc, scale):
         """
-            Print some information about how the normally-distributed
-            scaled parameters transform into "real" parameter space
+        Print some information about how the normally-distributed
+        scaled parameters transform into "real" parameter space
 
-            Args:
-                loc (torch.tensor):     scaled loc
-                scale (torch.tensor):   scaled scale
+        Args:
+            loc (torch.tensor):     scaled loc
+            scale (torch.tensor):   scaled scale
         """
         A = torch.log(self.u) - torch.log(self.l)
         B = torch.log(self.l)
@@ -186,15 +196,15 @@ class LogBoundedScalingFunction(ScalingFunction):
         mean = A * loc + B
         sp = A * scale
 
-        amean = torch.exp(mean + sp**2/2.0)
+        amean = torch.exp(mean + sp**2 / 2.0)
 
         msg = ""
         msg += "Log normal distribution with:\n"
         msg += "\tloc:\n"
-        msg += "\t"+str(mean) + "\n"
+        msg += "\t" + str(mean) + "\n"
         msg += "\tscale:\n"
-        msg += "\t"+str(sp)+"\n"
+        msg += "\t" + str(sp) + "\n"
         msg += "\tand mean values:\n"
-        msg += "\t"+str(amean)
+        msg += "\t" + str(amean)
 
         return msg
--- test/test_scaling.py
+++ test/test_scaling.py
@@ -4,6 +4,7 @@ import torch
 
 from pyoptmat import scaling
 
+
 class ScalingTests:
     def test_forward_back(self):
         actual = self.f.scale(self.x)
@@ -14,6 +15,7 @@ class ScalingTests:
 
         self.assertTrue(torch.allclose(self.x, back))
 
+
 class TestBoundedScalingFunction(unittest.TestCase, ScalingTests):
     def setUp(self):
         self.l = torch.tensor([2.0, -1.0])
@@ -23,9 +25,10 @@ class TestBoundedScalingFunction(unittest.TestCase, ScalingTests):
 
         self.f = scaling.BoundedScalingFunction(self.l, self.u)
 
+
 class TestLogBoundedScalingFunction(unittest.TestCase, ScalingTests):
     def setUp(self):
-        self.l = torch.tensor([2.0,  0.5])
+        self.l = torch.tensor([2.0, 0.5])
         self.u = torch.tensor([12.0, 2.0])
 
         self.x = torch.tensor([0.6, 0.5])
