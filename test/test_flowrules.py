3130bda65e5aa7c61746f346084edad21b60549f
Mark Messner <mark.messner@gmail.com>
2022-11-30 17:32:14 -0600

    Well the problem is the rate sensitivity scaling

--- test/test_flowrules.py
+++ test/test_flowrules.py
@@ -64,7 +64,8 @@ class CommonFlowRule:
             lambda x: self.model.flow_rate(self.s, self.h, self.t, self.T, x)[0],
             self.erate,
         )
-        self.assertTrue(np.allclose(exact, torch.flatten(numer), rtol=1.0e-4))
+
+        self.assertTrue(np.allclose(exact, torch.flatten(numer), rtol=1.0e-2))
 
     def test_history_erate(self):
         if self.skip:
@@ -76,7 +77,7 @@ class CommonFlowRule:
             self.erate,
         )[..., 0]
 
-        self.assertTrue(np.allclose(exact, numer, rtol=1.0e-4))
+        self.assertTrue(np.allclose(exact, numer, rtol=1.0e-2, atol = 1e-6))
 
 
 class TestPerfectViscoplasticity(unittest.TestCase, CommonFlowRule):
@@ -104,9 +105,18 @@ class TestWrappedRIIsoKinViscoplasticity(unittest.TestCase, CommonFlowRule):
 
         self.nbatch = 10
 
-        self.R = torch.tensor(101.0)
-        self.d = torch.tensor(1.3)
-        self.iso = hardening.VoceIsotropicHardeningModel(CP(self.R), CP(self.d))
+        self.tau = torch.tensor(100.0)
+        self.theta = torch.tensor(12.0)
+        self.r1 = 0.1
+        self.r2 = 1.2
+        self.R0 = 10.0
+        self.iso = hardening.Theta0RecoveryVoceIsotropicHardeningModel(
+            CP(self.tau), CP(self.theta), CP(self.R0), CP(self.r1), CP(self.r2)
+        )
+
+        self.C1 = torch.tensor(1200.0)
+        self.g1 = torch.tensor(10.1)
+        self.kin1 = hardening.FAKinematicHardeningModel(CP(self.C1), CP(self.g1))
 
         self.C = torch.tensor(1200.0)
         self.g = torch.tensor(10.1)
@@ -189,13 +199,13 @@ class TestKocksMeckingRegimeFlowRule(unittest.TestCase, CommonFlowRule):
             self.model1, self.model2, self.g0, self.mu, self.b, self.eps0, self.k
         )
 
-        self.s = torch.linspace(150, 200, self.nbatch)
+        self.s = torch.linspace(150, 200, self.nbatch) * 10
         self.h = torch.reshape(
             torch.tensor(
                 np.array(
                     [
-                        np.linspace(51, 110, self.nbatch),
-                        np.linspace(-100, 210, self.nbatch)[::-1],
+                        np.linspace(51, 110, self.nbatch)/10.0,
+                        np.linspace(-100, 210, self.nbatch)[::-1]/10.0,
                     ]
                 )
             ).T,
@@ -250,7 +260,7 @@ class TestSoftKocksMeckingRegimeFlowRule(unittest.TestCase, CommonFlowRule):
         self.k = 1.0
 
         self.g0 = 1.5
-        self.sf = 100.0
+        self.sf = 10.0
 
         self.model = flowrules.SoftKocksMeckingRegimeFlowRule(
             self.model1, self.model2, self.g0, self.mu, self.b, self.eps0, self.k, self.sf
@@ -263,6 +273,72 @@ class TestSoftKocksMeckingRegimeFlowRule(unittest.TestCase, CommonFlowRule):
                     [
                         np.linspace(51, 110, self.nbatch),
                         np.linspace(-100, 210, self.nbatch)[::-1],
+                    ]
+                )
+            ).T,
+            (self.nbatch, 2),
+        )
+
+        self.t = torch.ones(self.nbatch)
+        self.T = torch.ones_like(self.t) * 300
+        self.erate = torch.linspace(1e-2, 1e-3, self.nbatch)
+
+        self.skip = False
+
+class TestSoftKocksMeckingRegimeFlowRuleComplex(unittest.TestCase, CommonFlowRule):
+    def setUp(self):
+        self.nbatch = 10
+
+        self.n1 = torch.tensor(5.2)
+        self.eta1 = torch.tensor(110.0)
+        self.s01 = torch.tensor(50.0)
+
+        self.tau = torch.tensor(100.0)
+        self.theta = torch.tensor(12.0)
+        self.r1 = 0.1
+        self.r2 = 1.2
+        self.R0 = 10.0
+        self.iso1 = hardening.Theta0RecoveryVoceIsotropicHardeningModel(
+            CP(self.tau), CP(self.theta), CP(self.R0), CP(self.r1), CP(self.r2)
+        )
+
+        self.C1 = torch.tensor(1200.0)
+        self.g1 = torch.tensor(10.1)
+        self.kin1 = hardening.FAKinematicHardeningModel(CP(self.C1), CP(self.g1))
+
+        self.model1p = flowrules.IsoKinViscoplasticity(
+            CP(5.0), CP(200.0), CP(self.s01), self.iso1, self.kin1
+        )
+        eps0_ri = 1e-10
+        lmbda = 0.99
+        self.model1 = flowrules.RateIndependentFlowRuleWrapper(self.model1p, 
+                lmbda, eps0_ri)
+        
+        self.s02 = torch.tensor(50.0)
+        self.model2 = flowrules.IsoKinViscoplasticity(
+            CP(self.n1), CP(self.eta1), CP(0.0), self.iso1, self.kin1
+        )
+
+
+        self.mu = CP(1000.0)
+        self.b = 1.0
+        self.eps0 = 1.0
+        self.k = 1.0
+
+        self.g0 = 1.5
+        self.sf = 10.0
+
+        self.model = flowrules.SoftKocksMeckingRegimeFlowRule(
+            self.model1, self.model2, self.g0, self.mu, self.b, self.eps0, self.k, self.sf
+        )
+
+        self.s = torch.linspace(150, 200, self.nbatch)
+        self.h = torch.reshape(
+            torch.tensor(
+                np.array(
+                    [
+                        np.linspace(51, 110, self.nbatch)/10.0,
+                        np.linspace(-100, 210, self.nbatch)[::-1]/10.0,
                     ]
                 )
             ).T,
