2ca5b6d209a65c5940d5b10572a473945e900fe7
Mark Messner <mark.messner@gmail.com>
2023-08-31 13:27:01 -0500

    Increment version to catch up docs

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup (
     # Name of the project
     name = 'pyoptmat',
     # Version
-    version = '1.3.3',
+    version = '1.3.4',
     # One line-description
     description = "Statistical inference for material models",
     # README
